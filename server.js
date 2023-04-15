13babd358453a04f700d4ab5dab3178bc64c0d48
mahesh-mallareddy <mallareddymahesh0104@gmail.com>
2023-04-16 00:22:14 +0530

    updated menulist server

--- server.js
+++ server.js
@@ -37,6 +37,33 @@ app.get('/api/get_restaurants', (req, res) => {
 
 // ======================================== menulist
 
+app.get(':endpoint([\\/\\w\\.-]*)', (req, res) => {
+    // const { brand_id } = req.query;
+    // console.log(req.query);
+    const endpoint = `https://www.eatsure.com/v1/api${req.params.endpoint}`;
+  
+    fetch(endpoint, {
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
+      }
+    })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(data => {
+        res.json(data);
+      })
+      .catch(error => {
+        console.error(error);
+        res.status(500).send('An error occurred');
+      });
+  });
+
   // ===========================================port number
 const port = 2000;
 
